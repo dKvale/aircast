02cfc7932df88af1d9266eddcdea219802af6f02
Kvale <Dorian.Kvale@state.mn.us>
2019-05-16 08:39:05 -0500

    add Nick; round yesterday board digits

--- R/email_verification.R
+++ R/email_verification.R
@@ -29,7 +29,8 @@ aqi_team <- c(paste0(c("dorian.kvale",
                        "luke.charpentier",
                        "kari.palmer",
                        "monika.vadali",
-                       "helen.waquiu"), "@state.mn.us"), 
+                       "helen.waquiu",
+                       "nicholas.witcraft"), "@state.mn.us"), 
               "kvaled@gmail.com",
               "sirwin.mobile@gmail.com") 
 
